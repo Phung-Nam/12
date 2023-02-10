f0757fba0fb5c3b781607d71db4961142d2175e0
nam <phung9nam@gmail.com>
2023-02-10 07:40:04 +0700

    tinh nang 2 coommit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;//nhanh2
     return 0;
     // nhanh 1 phat trien tinh nang de push
+    //nhanh2 1
+    //nhanh2 2
+    //nhanh2 3
 }
